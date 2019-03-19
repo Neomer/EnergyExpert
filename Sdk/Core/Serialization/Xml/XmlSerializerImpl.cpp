560a550366d5358161c77dae7addbae70ff86b72
Vinokurov Kirill <vinokurov@elewise.com>
2019-03-19 12:08:54 +0400

    dev

--- Sdk/Core/Serialization/Xml/XmlSerializerImpl.cpp
+++ Sdk/Core/Serialization/Xml/XmlSerializerImpl.cpp
@@ -16,20 +16,25 @@ std::string XmlSerializerImpl::serialize(const energy::core::serialization::ISer
     auto node = static_cast<const XmlNode *>(object);
 
     std::stringstream stream;
-    stream << "<" << node->getName() << ">";
-    if (node->hasChildren()) {
-        std::for_each(node->getChildrenBeginIterator(), node->getChildrenEndIterator(), [this, &stream](const XmlNode *item) {
-            stream << serialize(item);
-        });
+    if (node->isEmpty()) {
+        stream << "<" << node->getName() << " />";
     } else {
-        stream << node->getText_const();
+        stream << "<" << node->getName() << ">";
+        if (node->hasChildren()) {
+            std::for_each(node->getChildrenBeginIterator(), node->getChildrenEndIterator(), [this, &stream](const XmlNode *item) {
+                stream << serialize(item);
+            });
+        } else {
+            stream << node->getText_const();
+        }
+        stream << "</" << node->getName() << ">";
     }
-    stream << "</" << node->getName() << ">";
 
     return std::string(stream.str());
 }
 
 void XmlSerializerImpl::deserialize(energy::core::serialization::ISerializable *object, std::string &data)
 {
+    //TODO: реализовать
 
 }
